3d473dcaa901a0b725295fa2bf94f222acbe369a
mr.li <onground@163.com>
2025-07-04 10:24:41 +0800

    update cfg file and its processing.

--- sysconfigs/sysconfigs.cpp
+++ sysconfigs/sysconfigs.cpp
@@ -8,40 +8,24 @@
 #undef ENUM_NAME_DEF
 #define ENUM_NAME_DEF(e) <<e
 
-static const char* gs_str_cfg_item = "配置项";
-static const char* gs_str_should_be_one_val_of = "应为如下值之一：";
-static const char* gs_str_actual_val = "实际值";
-static const char* gs_str_value_invalid = "值无效";
-
-static const char* gs_str_cfg_param_limit_error = "参数门限配置错误";
-static const char* gs_str_plz_check = "请检查！";
+static const char* gs_str_value_range = "取值范围";
 
 static const char* gs_sysconfigs_file_fpn = "configs/configs.ini";
 
 static const char* gs_ini_grp_sys_cfgs = "sys_cfgs";
 static const char* gs_ini_key_log_level = "log_level";
 
-static const char* gs_ini_grp_sc_data_cfg = "sc_data_cfg";
-static const char* gs_ini_key_max_pt_number = "max_pt_number";
-static const char* gs_ini_key_all_bytes_per_pt = "all_bytes_per_pt";
-static const char* gs_ini_key_pkt_idx_byte_cnt = "pkt_idx_byte_cnt";
-static const char* gs_ini_key_min_row_interval_ms = "min_row_interval_ms";
-static const char* gs_ini_key_max_row_interval_ms= "max_row_interval_ms";
-static const char* gs_ini_key_start_row_idx = "start_row_idx";
-static const char* gs_ini_key_row_idx_byte_order = "row_idx_byte_order";
-static const char* gs_ini_key_local_udp_port = "local_udp_port";
+static const char* gs_ini_grp_op_cfgs = "op_cfgs";
+static const char* gs_ini_key_sn_len = "sn_len";
+
+/*the max allowed sn len is limited by mem space in device factory sector.*/
+const int g_max_allowed_sn_len = 128; //8-bit ascii characters
 
 sys_configs_struct_t g_sys_configs_block;
 
 static const int gs_def_log_level = LOG_INFO;
 
-static const int gs_def_max_pt_number = 200, gs_def_all_bytes_per_pt = 3,
-                 gs_def_pkt_idx_byte_cnt = 2;
-static const int gs_def_min_row_interval_ms = 10, gs_def_max_row_interval_ms = 1000;
-static const int gs_def_start_row_idx = 0;
-static const byte_sent_order_e_t gs_def_row_idx_byte_order = LOW_BYTE_FIRST;
-
-static const quint16 gs_def_local_udp_port = 8020;
+static const int gs_def_sn_len = 20;
 
 static RangeChecker<int> gs_cfg_file_log_level_ranger((int)LOG_DEBUG, (int)LOG_ERROR, "",
                      EDGE_INCLUDED, EDGE_INCLUDED);
@@ -61,14 +45,20 @@ static RangeChecker<int> gs_cfg_file_value_01_int_ranger(0, 1, "",
 static RangeChecker<float> gs_cfg_file_value_gt0_float_ranger(0, 0, "",
                        EDGE_EXCLUDED, EDGE_INFINITE);
 
+static RangeChecker<int> gs_cfg_file_sn_ranger(0, g_max_allowed_sn_len, "",
+                       EDGE_INCLUDED, EDGE_INCLUDED);
+
 /*the __VA_ARGS__ should be empty or a type converter like (cust_type).*/
-#define GET_INF_CFG_NUMBER_VAL(settings, key, type_func, var, def, factor, checker, ...)\
+#define GET_INF_CFG_NUMBER_VAL(settings, key, type_func, var, def, factor, checker, checker_dtype, ...)\
 {\
+    cfg_ret = true; ret_str += (ret_str.isEmpty() ? "" : "\n");\
     (var) = __VA_ARGS__((factor) * ((settings).value((key), (def)).type_func()));\
     if((checker) && !((checker)->range_check((var))))\
     {\
-        (var) = (def);\
+        cfg_ret = false;\
+        ret_str += QString("%1:%2\n%3 %4").arg((key)).arg((var)).arg(gs_str_value_range, (checker)->range_str(checker_dtype));\
     }\
+    ret = ret && cfg_ret;\
 }
 
 /*check the validation of config parameters.*/
@@ -101,7 +91,7 @@ static RangeChecker<float> gs_cfg_file_value_gt0_float_ranger(0, 0, "",
 
 bool fill_sys_configs(QString * ret_str_ptr)
 {
-    bool ret = true, cfg_ret;
+    bool ret = true, cfg_ret = true;
     QString ret_str;
     QSettings settings(gs_sysconfigs_file_fpn, QSettings::IniFormat);
 
@@ -109,56 +99,19 @@ bool fill_sys_configs(QString * ret_str_ptr)
     settings.beginGroup(gs_ini_grp_sys_cfgs);
     GET_INF_CFG_NUMBER_VAL(settings, gs_ini_key_log_level, toInt,
                            g_sys_configs_block.log_level, gs_def_log_level,
-                           1, &gs_cfg_file_log_level_ranger);
+                           1, &gs_cfg_file_log_level_ranger, INT_DATA);
     settings.endGroup();
 
     /*--------------------*/
-    settings.beginGroup(gs_ini_grp_sc_data_cfg);
+    settings.beginGroup(gs_ini_grp_op_cfgs);
 
-    GET_INF_CFG_NUMBER_VAL(settings, gs_ini_key_max_pt_number, toInt,
-                           g_sys_configs_block.max_pt_number, gs_def_max_pt_number,
-                           1, &gs_cfg_file_value_gt0_int_ranger);
-
-    GET_INF_CFG_NUMBER_VAL(settings, gs_ini_key_all_bytes_per_pt, toInt,
-                           g_sys_configs_block.all_bytes_per_pt, gs_def_all_bytes_per_pt,
-                           1, &gs_cfg_file_value_gt0_int_ranger);
-
-    GET_INF_CFG_NUMBER_VAL(settings, gs_ini_key_pkt_idx_byte_cnt, toInt,
-                           g_sys_configs_block.pkt_idx_byte_cnt, gs_def_pkt_idx_byte_cnt,
-                           1, &gs_cfg_file_value_gt0_int_ranger);
-
-    GET_INF_CFG_NUMBER_VAL(settings, gs_ini_key_min_row_interval_ms, toInt,
-                           g_sys_configs_block.min_row_interval_ms, gs_def_min_row_interval_ms,
-                           1, (RangeChecker<int>*)0);
-    GET_INF_CFG_NUMBER_VAL(settings, gs_ini_key_max_row_interval_ms, toInt,
-                           g_sys_configs_block.max_row_interval_ms, gs_def_max_row_interval_ms,
-                           1, (RangeChecker<int>*)0);
-
-    GET_INF_CFG_NUMBER_VAL(settings, gs_ini_key_start_row_idx, toInt,
-                           g_sys_configs_block.start_row_idx, gs_def_start_row_idx,
-                           1, &gs_cfg_file_value_ge0_int_ranger);
-
-    GET_INF_CFG_NUMBER_VAL(settings, gs_ini_key_row_idx_byte_order, toInt,
-                           g_sys_configs_block.row_idx_byte_order, gs_def_row_idx_byte_order,
-                           1, (RangeChecker<int>*)0, (byte_sent_order_e_t));
-
-    GET_INF_CFG_NUMBER_VAL(settings, gs_ini_key_local_udp_port, toInt,
-                           g_sys_configs_block.local_udp_port, gs_def_local_udp_port,
-                           1, &gs_cfg_file_value_gt0_int_ranger, (quint16));
+    GET_INF_CFG_NUMBER_VAL(settings, gs_ini_key_sn_len, toInt,
+                           g_sys_configs_block.sn_len, gs_def_sn_len,
+                           1, &gs_cfg_file_sn_ranger, INT_DATA);
     settings.endGroup();
-    /*--------------------*/
-
-    if(!ret)  ret_str = QString(gs_str_cfg_param_limit_error) + "," + gs_str_plz_check + "\n" + ret_str;
 
     /*--------------------*/
-    QSet<byte_sent_order_e_t> byte_orders;
-    byte_orders BYTE_SENT_ORDER_E;
-    CHECK_ENUM(QString(gs_ini_key_row_idx_byte_order)  + gs_str_value_invalid + "," + gs_str_plz_check,
-               g_sys_configs_block.row_idx_byte_order,
-               byte_orders, QString::number)
 
-    /*--------------------*/
-    ret = ret && cfg_ret;
     if(ret_str_ptr) *ret_str_ptr = ret_str;
     return ret;
 }
--- sysconfigs/sysconfigs.h
+++ sysconfigs/sysconfigs.h
@@ -5,30 +5,14 @@
 
 #define ENUM_NAME_DEF(e) e,
 
-#define BYTE_SENT_ORDER_E \
-    ENUM_NAME_DEF(LOW_BYTE_FIRST) \
-    ENUM_NAME_DEF(HIGH_BYTE_FIRST)
-
-typedef enum
-{
-    BYTE_SENT_ORDER_E
-}byte_sent_order_e_t;
-
 typedef struct
 {
     int log_level;
 
-    int max_pt_number;
-    int all_bytes_per_pt;
-    int pkt_idx_byte_cnt;
-    int min_row_interval_ms;
-    int max_row_interval_ms;
-    int start_row_idx;
-    byte_sent_order_e_t row_idx_byte_order;
-
-    quint16 local_udp_port;
+    int sn_len;
 }sys_configs_struct_t;
 
+extern const int g_max_allowed_sn_len;
 extern sys_configs_struct_t g_sys_configs_block;
 
 bool fill_sys_configs(QString *);
